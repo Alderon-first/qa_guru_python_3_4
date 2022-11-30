8c91da4e9aa1ab7278f713b647b902f265fff72e
eushko <eushko@pikemedia.ru>
2022-11-30 11:35:19 +0300

    реализована функция, печатающая имя и параметры других функций

--- homework.py
+++ homework.py
@@ -2,8 +2,11 @@
 # читаемое имя переданной ей функции и значений аргументов.
 # Вызовите ее внутри функций, описанных ниже
 # Подсказка: Имя функции можно получить с помощью func.__name__
+import inspect
+
 
 def open_browser(browser_name):
+    return
     pass
 
 
@@ -13,3 +16,13 @@ def go_to_companyname_homepage(page_url):
 
 def find_registration_button_on_login_page(page_url, button_text):
     pass
+
+
+def print_inf_fnc(fnc):
+    for element in fnc:
+        print("имя функции: " + element.__name__)
+        print("атрибуты функции: " + str(inspect.signature(element))[1:-1])
+
+
+fnc = [find_registration_button_on_login_page, open_browser, go_to_companyname_homepage]
+print_inf_fnc(fnc)
