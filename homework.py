589be236c26ee228daf2f82c33e965029ad59272
eushko <eushko@pikemedia.ru>
2022-12-01 13:56:58 +0300

    форматирование кода

--- homework.py
+++ homework.py
@@ -12,49 +12,56 @@
 
 import inspect
 
-
 print("---------")
+
+
 def print_inf_fnc(search_arg, *args):
-    i=0
-    search_arg = search_arg.__name__.capitalize().replace("_", " ") # получаю имя функции из параметра
-    print(f"Имя функции:  {search_arg}") # вывожу имя функции
+    i = 0
+    search_arg = search_arg.__name__.capitalize().replace("_", " ")
+    # получаю имя функции из параметра
+    print(f"Имя функции:  {search_arg}")  # вывожу имя функции
     print("Аргументы функции :")
-    while i < len(args): # беру список args  и по индексу вывожу парами. предполагаю, что список аргументов, взятый из
-        #из исходной функции всегда == по длинне списку знаечний агрументов, который я получаю как args,
+    while i < len(args):
+        # беру список args  и по индексу вывожу парами. предполагаю, что список аргументов, взятый из
+        # из исходной функции всегда == по длинне списку знаечний агрументов, который я получаю как args,
         # хотя второй это картеж (кажется), а первый - массив
         print("--" + o[i] + ": " + args[i])
-        i += 1 # увеличиваю счетчик
+        i += 1  # увеличиваю счетчик
     print("-----------")
+
 
 def open_browser(browser_name):
     global o
-    o = inspect.getfullargspec(open_browser).args #тут получаю список аргументов в виде массива
+    # глобальная нужна, чтобы значение прокинуть глубже, в функцию принта
+    o = inspect.getfullargspec(open_browser).args  # тут получаю список аргументов в виде массива
     print_inf_fnc(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
     global o
-    o = inspect.getfullargspec(go_to_companyname_homepage).args  # тут получаю список аргументов в виде массива
+    o = inspect.getfullargspec(go_to_companyname_homepage).args
+    # тут получаю список аргументов в виде массива
     print_inf_fnc(go_to_companyname_homepage, page_url)
 
 
-def find_registration_button_on_login_page(page_url, button_text, test='test1'):
+def find_registration_button_on_login_page(page_url, button_text):
     global o
-    o = inspect.getfullargspec(find_registration_button_on_login_page).args # тут получаю список аргументов в виде массива
+    o = inspect.getfullargspec(find_registration_button_on_login_page).args
+    # тут получаю список аргументов в виде массива
     print_inf_fnc(go_to_companyname_homepage, page_url, button_text)
 
 
-#-----
-#работает, но не принято преподавателем
-#def print_inf_fnc(fnc_name):
-    #for element in fnc:
-        #print("имя функции: " + element.__name__)
-        #print("атрибуты функции: " + str(inspect.signature(element))[1:-1])
+# -----
+# работает, но не принято преподавателем
+# def print_inf_fnc(fnc_name):
+# for element in fnc:
+# print("имя функции: " + element.__name__)
+# print("атрибуты функции: " + str(inspect.signature(element))[1:-1])
 
 
-#fnc = [find_registration_button_on_login_page, open_browser, go_to_companyname_homepage]
-#print_inf_fnc(fnc)
-#-----
+# fnc = [find_registration_button_on_login_page, open_browser, go_to_companyname_homepage]
+# print_inf_fnc(fnc)
+# -----
 
 open_browser(browser_name="Chrome")
 go_to_companyname_homepage(page_url="https://github.com")
